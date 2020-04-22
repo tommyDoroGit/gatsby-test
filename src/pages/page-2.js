9e2d58374f4284fbfc643ff6c82a250113a0ad95
tommyDoroGit <40597861+tommyDoroGit@users.noreply.github.com>
2020-04-22 16:31:00 +0800

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,7 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the homepage</Link><br/>
     <Link to="/404">Go to 404</Link>
   </Layout>
 )
