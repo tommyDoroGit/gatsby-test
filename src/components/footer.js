950ccd55db29c3baa939b09e687dc7b86c70b8e4
tommyDoroGit <40597861+tommyDoroGit@users.noreply.github.com>
2020-05-04 17:24:28 +0800

    Footer Component Added to Layout

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer>
+        <div>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </div>
+    </footer>
+    )
+
+    export default Footer
+
